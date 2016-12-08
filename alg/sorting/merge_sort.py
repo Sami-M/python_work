5e4fff504d9198748834dc0cfa0fa016904d3978
Sami-M <sami.mirimiri@live.com>
2016-12-07 20:14:18 -0500

    added merge sort

---
+++ alg/sorting/merge_sort.py
@@ -0,0 +1,36 @@
+"""
+Created by Sami on 12/6/2016
+"""
+
+def merge_sort(unsorted_list):
+    print("sorting", unsorted_list)
+    if(len(unsorted_list) != 1):
+        middle = len(unsorted_list) // 2
+        leftArray = unsorted_list[0:middle]
+        rightArray = unsorted_list[middle:]
+        merge_sort(leftArray)
+        merge_sort(rightArray)
+        i = 0
+        j = 0
+        k = 0
+        print('merging ', unsorted_list)
+        while (i < len(leftArray) and j < len(rightArray)):
+            if(leftArray[i] < rightArray[j]):
+                unsorted_list[k] = leftArray[i]
+                i = i + 1
+            else:
+                unsorted_list[k] = rightArray[j]
+                j = j + 1
+            k = k + 1
+        while (i < len(leftArray)):
+            unsorted_list[k] = leftArray[i]
+            i = i + 1
+            k = k + 1
+        while (j < len(rightArray)):
+            unsorted_list[k] = rightArray[j]
+            j = j + 1
+            k = k + 1
+
+list_to_sort = [4,8,2,5,1,9]
+merge_sort(list_to_sort)
+print(list_to_sort)
